551234e0a82abbc3774673ee7848d03d0e2a4fd7
Phelyppe Álex <phelyppealex@pop-os.localdomain>
2022-10-20 21:05:19 -0300

    mapa e boneco à postos

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let canvas = document.getElementById('canvas')
 let c = canvas.getContext('2d')
-console.log(canvas)
 
 //Definindo largura do canvas
 canvas.width = 960
@@ -8,13 +7,23 @@ canvas.height = 540
 
 //Criando objeto da imagem do mapa
 const imagem = new Image()
-imagem.src = 'map.png'
+imagem.src = 'img/map.png'
 
 //Criando objeto da imagem do boneco
 const jogadorImagem = new Image()
-jogadorImagem.src = '/images/playerDown.png'
+jogadorImagem.src = 'img/playerDown.png'
 
 imagem.onload = () => {
     c.drawImage(imagem,0,-150)
-    c.drawImage(jogadorImagem,0,0)
+    c.drawImage(
+        jogadorImagem,
+        0,
+        0,
+        jogadorImagem.width/4,
+        jogadorImagem.height,
+        canvas.width/2 - jogadorImagem.width/2,
+        canvas.height/2 - jogadorImagem.height/2,
+        jogadorImagem.width/4,
+        jogadorImagem.height
+    )
 }
